fb2b4f01bca981e17f3adc122aa77facaae383cc
Fabio Frattarelli <fabiofrt@outlook.it>
2023-02-12 11:09:51 +0100

    commit

--- src/main/java/com/example/Deploy_01/controllers/DevController.java
+++ src/main/java/com/example/Deploy_01/controllers/DevController.java
@@ -2,9 +2,11 @@ package com.example.Deploy_01.controllers;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/name")
 public class DevController {
 
     @GetMapping("/")
