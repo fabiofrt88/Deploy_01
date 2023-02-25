e2f31a21a7f89a34a3557396cb072dbdd0d8fc0e
Fabio Frattarelli <fabiofrt@outlook.it>
2023-02-25 10:19:53 +0100

    commit

--- src/main/java/com/example/Deploy_01/controllers/DevController.java
+++ src/main/java/com/example/Deploy_01/controllers/DevController.java
@@ -1,6 +1,8 @@
 package com.example.Deploy_01.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/name")
 public class DevController {
 
-    @GetMapping("/")
+    @Autowired
+    public Environment environment;
+
+    @GetMapping("/get")
     public String getDevName(){
-        return "Fabio";
+        return environment.getProperty("devName");
     }
 }
